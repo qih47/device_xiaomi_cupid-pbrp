7630efe68816f17c63a20c923821b8db169f4a58
Qisthi Iskandar Haqiki <qisthih@gmail.com>
2022-06-01 23:27:51 +0700

    CUPID : Initial PBRP A11

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_cupid.mk
+
+COMMON_LUNCH_CHOICES := \
+     omni_cupid-eng
